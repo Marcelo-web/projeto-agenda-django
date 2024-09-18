17b7655d0e25f8e3df2fd076b7be75193fa7f94d
Marcelo <marestdesenv@gmail.com>
2024-09-18 12:27:03 -0300

    ForeignKey - criando o model Category e fazendo a ligacao com chave estrangeira

--- contact/admin.py
+++ contact/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from contact import models
 
 
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
+
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
@@ -12,4 +18,3 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 1
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
-
