4544a601bc10e7dda4bfdefe16e2ab25a61eef0a
Marcelo <marestdesenv@gmail.com>
2024-09-26 12:46:45 -0300

    Criando arquivos parciais para usar com include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
-# from django.http import Http404
 
 
 # Create your views here.
@@ -9,7 +8,8 @@ def index(request):
                    .filter(show=True) \
                    .order_by('-id')[10:20]
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -20,14 +20,13 @@ def index(request):
 
 
 def contact(request, contact_id):
-    # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
-    # if single_contact is None:
-    #     raise Http404()
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
