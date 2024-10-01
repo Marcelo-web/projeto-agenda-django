e53744c880adae285e4939755729c7fd11b01625
Marcelo <marestdesenv@gmail.com>
2024-10-01 00:17:01 -0300

    Usando o request.method e request.POST para saber quando o formulario esta sendo postado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 
 def create(request):
+    if request.method == 'POST':
+        print(request.method)
+        print(request.POST.get('first_name'))
+
     context = {
     }
+
+    print(request.method)
 
     return render(
         request,
